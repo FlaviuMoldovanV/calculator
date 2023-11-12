11a5a27a71235701ed52c60c6029e74f6df4e704
FlaviuMoldovanV <69804853+FlaviuMoldovanV@users.noreply.github.com>
2023-11-12 15:44:42 +0200

    feat: Create calculator UI and base functions

---
+++ calculator.js
@@ -0,0 +1,22 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+
+const operate = (operator, firstNumber, secondNumber) => {
+
+    switch (operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+        case "-":
+            return subtract(firstNumber, secondNumber);
+        case "*":
+            return multiply(firstNumber, secondNumber);
+        case "/":
+            return divide(firstNumber, secondNumber);
+    }
+}
