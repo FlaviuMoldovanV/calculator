e4646221702ae0ede6e334500f17530c68f3badc
FlaviuMoldovanV <69804853+FlaviuMoldovanV@users.noreply.github.com>
2023-11-12 19:15:14 +0200

    feat: Functional calculator

--- calculator.js
+++ calculator.js
@@ -1,13 +1,22 @@
+"use strict";
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const divide = (a, b) => parseFloat((a / b).toFixed(3));
 
-let firstNumber = 0;
-let secondNumber = 0;
-let operator = "";
+let firstNumber = "";
+let secondNumber = "";
+let symbol = "";
+let total = "";
 
-const operate = (operator, firstNumber, secondNumber) => {
+const display = document.querySelector(".display");
+const resultDisplay = document.querySelector(".resultDisplay")
+
+function operation(firstNumber, secondNumber, operator) {
+
+    firstNumber = parseFloat(firstNumber);
+    secondNumber = parseFloat(secondNumber);
 
     switch (operator) {
         case "+":
@@ -20,3 +29,60 @@ const operate = (operator, firstNumber, secondNumber) => {
             return divide(firstNumber, secondNumber);
     }
 }
+
+function numberPress(input) {
+
+    if (symbol == "") {
+        firstNumber += input;
+        display.textContent = firstNumber;
+    }
+
+    else {
+        secondNumber += input;
+        display.textContent += input;
+    }
+}
+
+function operatorPress(input) {
+    if (symbol == "") {
+        symbol = input;
+        display.textContent += ` ${input} `;
+    }
+    else {
+        total = operation(firstNumber, secondNumber, symbol);
+        firstNumber = total;
+        secondNumber = "";
+        symbol = input;
+        display.textContent += ` ${input} `;
+        resultDisplay.textContent = total;
+    }
+}
+
+function equalPress() {
+    total = operation(firstNumber, secondNumber, symbol);
+    firstNumber = total;
+    secondNumber = "";
+    symbol = "";
+    resultDisplay.textContent = "";
+    display.textContent = total;
+
+
+}
+
+
+const numberButtons = document.querySelectorAll(".number");
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => numberPress(button.textContent));
+});
+
+
+const operatorButtons = document.querySelectorAll(".operator");
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => operatorPress(button.textContent));
+});
+
+
+const equal = document.querySelector(".equal");
+equal.addEventListener("click", () => equalPress());
